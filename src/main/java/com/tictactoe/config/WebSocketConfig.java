61274dfbd7569f3ed06b5b2e7e4b0a30fe1ab63d
Tuckens <62431981+Tuckens@users.noreply.github.com>
2026-01-07 19:14:13 +0100

    changing WebSocketConfig

--- src/main/java/com/tictactoe/config/WebSocketConfig.java
+++ src/main/java/com/tictactoe/config/WebSocketConfig.java
@@ -12,18 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-
         config.enableSimpleBroker("/topic");
-
-
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
         registry.addEndpoint("/ws-tictactoe")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
